e29e05e404afbbec2710d1be4af1026ca9151e15
Shawn Kristek <shawnkristek@gmail.com>
2022-07-18 09:11:02 -0600

    amended views to use django generic

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from .models import Question, Choice
@@ -19,22 +19,6 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = { 'latest_question_list': latest_question_list }
-    return render(request, 'polls/index.html', context)
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
