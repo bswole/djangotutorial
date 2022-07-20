9a927eaf9112e540b009c40dd41bb955bb7352da
Shawn Kristek <shawnkristek@gmail.com>
2022-07-20 11:41:49 -0500

    updates to admin search and sort

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-class ChoiceInLine(admin.StackedInline):
+class ChoiceInLine(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -11,5 +11,10 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date Information',    {'fields': ['pub_date'], 'classes':['collapse']}),
     ]
     inlines = [ChoiceInLine]
+    
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
